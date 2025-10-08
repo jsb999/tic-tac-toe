a31ba3bb38a9d50337ee4ee28ce3bdffb1c338f2
jsb999 <90780236+jsb999@users.noreply.github.com>
2025-10-08 22:19:41 +0000

    change

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -4,4 +4,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
+  @Test
+  void testBoard() {
+    Board board = new Board();
+    assertEquals(1, board.checkWin());
+    board.setGridSquare(0, -1);
+    board.setGridSquare(1, -1);
+    board.setGridSquare(2, -1);
+    assertEquals(-1, board.checkWin());
+  }
 }
